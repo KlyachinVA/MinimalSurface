00b2750f9fef25a3acee4f691e50abafe1ac4b71
Klyachin Vladimir <klchnv@mail.ru>
2016-05-30 22:31:45 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import bpy
+from . import panel_addon
+bl_info = { 
+    "name": "Construct minimal surfaces addon",
+    "category": "3D View",
+	"author":"Klyachin V.A., Grigorieva E.G. VolSU,2016"
+}
+
+def register():		
+  bpy.utils.register_class(panel_addon.MyPanel)
+  bpy.utils.register_class(panel_addon.FindBoundary)
+  bpy.utils.register_class(panel_addon.FindMinSurface)
+  bpy.utils.register_class(panel_addon.FindTentSurface)  
+  bpy.utils.register_class(panel_addon.Settings)
+def unregister():	
+  bpy.utils.unregister_class(panel_addon.MyPanel)
+  bpy.utils.unregister_class(panel_addon.FindBoundary)
+  bpy.utils.unregister_class(panel_addon.FindMinSurface)
+  bpy.utils.unregister_class(panel_addon.FindTentSurface)
+  bpy.utils.unregister_class(panel_addon.Settings)  
+# if __name__ == "__main__":
+  # register()
