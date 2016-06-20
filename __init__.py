665241eecc8bddfcbda7260e0ae88f5ccf92bf7c
Klyachin Vladimir <klchnv@mail.ru>
2016-06-20 16:27:46 +0300

    /old

--- __init__.py
+++ __init__.py
@@ -1,26 +1,18 @@
 import bpy
 from . import panel_addon
 bl_info = { 
-    "name": "Construct minimal surfaces addon",
+    "name": "Find Boundary Addon",
     "category": "3D View",
 	"author":"Klyachin V.A., Grigorieva E.G. VolSU,2016"
 }
 
 def register():		
-  bpy.utils.register_class(panel_addon.MinimalSurfacePanel)
+  bpy.utils.register_class(panel_addon.MyPanel)
   bpy.utils.register_class(panel_addon.FindBoundary)
-  bpy.utils.register_class(panel_addon.FindMinSurface)
-  bpy.utils.register_class(panel_addon.FindCMCSurface)
-  bpy.utils.register_class(panel_addon.FindCapillarSurface)
-  bpy.utils.register_class(panel_addon.FindTentSurface)  
-  bpy.utils.register_class(panel_addon.Settings)
+  bpy.utils.register_class(panel_addon.FindMinSurface) 
 def unregister():	
-  bpy.utils.unregister_class(panel_addon.MinimalSurfacePanel)
+  bpy.utils.unregister_class(panel_addon.MyPanel)
   bpy.utils.unregister_class(panel_addon.FindBoundary)
-  bpy.utils.unregister_class(panel_addon.FindMinSurface)
-  bpy.utils.unregister_class(panel_addon.FindCMCSurface)
-  bpy.utils.unregister_class(panel_addon.FindCapillarSurface)
-  bpy.utils.unregister_class(panel_addon.FindTentSurface)
-  bpy.utils.unregister_class(panel_addon.Settings)  
+  bpy.utils.unregister_class(panel_addon.FindMinSurface) 
 # if __name__ == "__main__":
   # register()
